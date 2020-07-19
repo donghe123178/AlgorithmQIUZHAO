a53adfd3524c92198fd3938a6957e91d95e36fc1
donghe <lianxiang237@qq.com>
2020-07-19 21:01:46 +0800

    第1遍

---
+++ Week_01/leetcode/editor/cn/[21]合并两个有序链表.py
@@ -0,0 +1,61 @@
+# 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+# 
+#  
+# 
+#  示例： 
+# 
+#  输入：1->2->4, 1->3->4
+# 输出：1->1->2->3->4->4
+#  
+#  Related Topics 链表 
+#  👍 1155 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        """
+        1.递归
+        终止条件：当出现链表有一个为空
+        时间复杂度：O(M+N)
+        空间复杂度：O(m+n)
+        """
+        # if l1 is None:
+        #     return l2
+        # elif l2 is None:
+        #     return l1
+        # elif l1.val < l2.val:
+        #     l1.next =self.mergeTwoLists(l1.next, l2)
+        #     return l1
+        # else:
+        #     l2.next = self.mergeTwoLists(l1, l2.next)
+        #     return l2
+        """
+        2.迭代
+        时间复杂度:O(n+m)
+        空间复杂度：O(1)
+        """
+        prehead = ListNode(-1)
+        pre = prehead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                pre.next = l1
+                l1 = l1.next
+            else:
+                pre.next = l2
+                l2 = l2.next
+            pre = pre.next
+        pre.next = l1 if l1 is not None else l2
+        return prehead.next
+
+# leetcode submit region end(Prohibit modification and deletion)
