2b5d49089d18dacbde9efae7b0afcd1d79c604ac
donghe <lianxiang237@qq.com>
2020-07-21 09:15:42 +0800

    第3遍

---
+++ Week_02/leetcode/editor/cn/[144]二叉树的前序遍历.py
@@ -0,0 +1,64 @@
+# 给定一个二叉树，返回它的 前序 遍历。 
+# 
+#  示例: 
+# 
+#  输入: [1,null,2,3]  
+#    1
+#     \
+#      2
+#     /
+#    3 
+# 
+# 输出: [1,2,3]
+#  
+# 
+#  进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+#  Related Topics 栈 树 
+#  👍 313 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        # if not root:
+        #     return []
+        # return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+
+        # 递归第二种方法
+        # def dfs(root):
+        #     if not root:
+        #         return None
+        #     if root:
+        #         res.append(root.val)
+        #         dfs(root.left)
+        #         dfs(root.right)
+        # res =[]
+        # dfs(root)
+        # return res
+
+        # 迭代
+        if not root:
+            return []
+        res = []
+        stack = [root]
+        while stack:
+            tmp = stack.pop()
+            res.append(tmp.val)
+            if tmp.right:
+                stack.append(tmp.right)
+            if tmp.left:
+                stack.append(tmp.left)
+        return res
+
+# leetcode submit region end(Prohibit modification and deletion)
